143538eca29bdb69ceeec278a846e0150fa7d318
0000aditya0000 <adityagangwar516@gmail.com>
2026-09-11 21:45:46 +0530

    payout secret

--- src/routes/payout.routes.js
+++ src/routes/payout.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const payoutController = require('../controllers/payout.controller');
 const { validate } = require('../middlewares/validation');
 const { payoutCreateSchema, queryOrderSchema } = require('../validators/aeropay.validator');
+const { requirePayoutSecret } = require('../middlewares/requirePayoutSecret');
 
 const router = express.Router();
 
-router.post('/create', validate(payoutCreateSchema), payoutController.createPayout);
+router.post('/create', requirePayoutSecret, validate(payoutCreateSchema), payoutController.createPayout);
 router.post('/status', validate(queryOrderSchema), payoutController.queryPayout);
 router.get('/balance', payoutController.merchantBalance);
 router.post('/balance', payoutController.merchantBalance);
